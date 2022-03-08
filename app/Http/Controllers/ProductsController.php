0a3698dcba1d16186d577ee21a7821ee61ba7a72
warisabdullah <waris@softwarealliance.dk>
2022-03-08 17:15:29 +0500

    Waris Push Branch

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -96,7 +96,7 @@ class ProductsController extends Controller
            'price' => $request->price,
            'is_active' => $request->is_active,
        ];
-       //
+       //d
         $productUpdated = $this->productRepository->updateProduct($request->_id, $data);
         return response()->json("Success");
     }
